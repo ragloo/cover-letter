f203ceb8f0615a742f4553e778cac69fc43313f5
BipBip Yeah <ragloo@users.noreply.github.com>
2021-04-19 22:30:01 +0200

    Add files via upload

--- cover-letter.ts
+++ cover-letter.ts
@@ -37,8 +37,8 @@
 
   <assign|page-even|1.4cm>
 
-  <assign|page-even-footer|<small|<name|<date|>><htab|5mm><name|Claud D. Park
-  - Cover Letter>><htab|5mm>>
+  <assign|page-even-footer|<very-small|<name|<date|>><htab|5mm><name|<firstname>
+  <lastname> - Cover Letter><htab|5mm>>>
 
   <assign|page-even-header|>
 
@@ -46,8 +46,8 @@
 
   <assign|page-odd|1.4cm>
 
-  <assign|page-odd-footer|<small|<name|<date|>><htab|5mm><name|Claud D. Park
-  - Cover Letter>><htab|5mm>>
+  <assign|page-odd-footer|<very-small|<name|<date|>><htab|5mm><name|<firstname>
+  <lastname> - Cover Letter><htab|5mm>>>
 
   <assign|page-odd-header|>
 
@@ -65,24 +65,69 @@
     </src-comment>
   </active*>
 
+  <assign|firstname|Claud D.>
+
+  <assign|lastname|Park>
+
+  <assign|email|posquit0.bj@gmail.com >
+
+  <assign|webpage|www.posquit0.com >
+
+  <assign|phone|(+82) 10-9030-1843 >
+
+  <assign|github|posquit0>
+
+  <assign|linkedIn|posquit0>
+
   \;
 
-  <assign|section*|<macro|title|<assign|section-numbered|false><assign|section-prefix|<macro|>><compound|section-clean><compound|section-header|<arg|title>><compound|section-toc|<arg|title>><compound|section-unnumbered-title|<arg|title><underline|<htab|5mm>>>>>
+  <assign|section*|<macro|title|<assign|section-numbered|false><assign|section-prefix|<macro|>><compound|section-clean><compound|section-header|<arg|title>><compound|section-toc|<arg|title>><compound|section-unnumbered-title|<arg|title><underline|<htab|0mm>>>>>
 
-  <assign|encl|<macro|body|<vspace*|0.5fn><localize|Encl>:
-  <tabular|<tformat|<cwith|1|1|1|1|cell-lsep|0spc>|<cwith|1|1|1|1|cell-rsep|0spc>|<cwith|1|1|1|1|cell-hyphen|n>|<twith|table-valign|T>|<table|<row|<\cell>
-    <encl*|<arg|body>>
-  </cell>>>>><right-flush>>>
+  \;
 
-  <assign|signature|<macro|body|<surround|<vspace*|2fn>||<signature*|<with|font-series|bold|<arg|body>>>>>>
+  <assign|destination-name|<macro|body|<with|font-series|bold|<arg|body>>>>
 
-  <assign|closing|<macro|body|<\surround||<right-flush><vspace|2fn>>
-    <closing*|<arg|body>>
-  </surround>>>
+  \;
+
+  <assign|destination-address|<\macro|body>
+    <with|par-par-sep|0.333fn|<very-small|<name|<arg|body>>>>
+  </macro>>
+
+  \;
 
   <assign|object|<macro|body|<\surround|<vspace*|1fn>|<vspace|1fn>>
-    <with|font-series|bold|<underline|Object: <arg|body>>>
+    <with|font-series|bold|<underline|<arg|body>>>
   </surround>>>
+
+  \;
+
+  <assign|opening|<macro|body|<\surround|<vspace*|1fn>|<right-flush><vspace|1fn>>
+    <arg|body>
+  </surround>>>
+
+  \;
+
+  <assign|closing|<macro|body|<\surround|<vspace*|1fn>|<right-flush><vspace|1fn>>
+    <arg|body>
+  </surround>>>
+
+  \;
+
+  <assign|signature|<macro|body|<surround|<vspace*|1fn>||<with|font-series|bold|<arg|body>>>>>
+
+  <assign|sender-name|<macro|firstname|lastname|<really-huge|<arg|firstname>
+  <with|font-series|bold|<arg|lastname>>>>>
+
+  <assign|sender-title|<macro|body|<name|<small|<with|color|red|<arg|body>>>>>>
+
+  <assign|sender-address|<macro|body|<compound|small|<arg|body>>>>
+
+  <assign|sender-citation|<macro|body|<surround|<vspace*|2fn>||<small|<with|font-shape|italic|<arg|body>>>>>>
+
+  <assign|encl|<macro|body|<surround|<vspace*|1fn>||<with|font-shape|italic|<localize|Encl>:
+  <arg|body>>>>>
+
+  \;
 </body>
 
 <initial|<\collection>
